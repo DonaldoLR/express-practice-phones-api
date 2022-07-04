12982cea3dea591a7c4c4fccd1051b68dc73baa5
Donaldo Lara-Reyes <bonelessdarkmatter_code@protonmail.com>
2022-07-04 17:36:19 -0400

    create & test phone patch route

--- controllers/phoneController.js
+++ controllers/phoneController.js
@@ -51,10 +51,27 @@ const deletePhone = async (req, res) => {
 	res.status(200).json({ message: 'Phone deleted', phone });
 };
 // UPDATE a phone
+const updatePhone = async (req, res) => {
+	const { id } = req.params;
 
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: 'No such phone' });
+	}
+
+	const updatedPhone = await Phone.findByIdAndUpdate(id, {
+		...req.body,
+	});
+
+	if (!updatePhone) {
+		return res.status(404).json({ error: 'No such phone' });
+	}
+
+	res.status(200).json(updatedPhone);
+};
 module.exports = {
 	getPhones,
 	getPhone,
 	createPhone,
 	deletePhone,
+	updatePhone,
 };
--- routes/phones.js
+++ routes/phones.js
@@ -5,10 +5,12 @@ const {
 	getPhone,
 	createPhone,
 	deletePhone,
+	updatePhone,
 } = require('../controllers/phoneController');
 
 router.get('/', getPhones);
 router.get('/:id', getPhone);
 router.post('/', createPhone);
 router.delete('/:id', deletePhone);
+router.patch('/:id', updatePhone);
 module.exports = router;
