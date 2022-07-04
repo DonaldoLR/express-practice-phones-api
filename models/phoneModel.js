eed88a25ffcffd226ee5d1160b52480046eb5885
Donaldo Lara-Reyes <bonelessdarkmatter_code@protonmail.com>
2022-07-04 12:48:34 -0400

    add timestamps to schema

--- models/phoneModel.js
+++ models/phoneModel.js
@@ -1,19 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const phoneModel = new Schema({
-	name: {
-		type: String,
-		required: true,
+const phoneModel = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		release_date: {
+			type: Date,
+			required: false,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
 	},
-	release_date: {
-		type: Date,
-		required: false,
-	},
-	price: {
-		type: Number,
-		required: true,
-	},
-});
+	{ timestamps: true }
+);
 
 module.exports = phoneModel;
