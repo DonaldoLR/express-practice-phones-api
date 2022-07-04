3bdcf6cdab9c88db1843c3a58281f447e78a88eb
Donaldo Lara-Reyes <bonelessdarkmatter_code@protonmail.com>
2022-07-04 12:47:51 -0400

    setup server file and connect to mongodb. Create phone model schema

---
+++ models/phoneModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const phoneModel = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	release_date: {
+		type: Date,
+		required: false,
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+});
+
+module.exports = phoneModel;
---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Start a server
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use((req, res) => {
+	console.log(req.path, req.method);
+});
+
+// Connect to DB
+
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+		app.listen(process.env.PORT, () => {
+			console.log('Connected to DB');
+		});
+	})
+	.catch((error) => console.log(error));
